e4375b6d3fbfe4bac173237a8eda2265245a5bea
yurekli <kurumizawa38@gmail.com>
2025-01-07 21:57:40 -0400

    Updated Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,26 @@
-import continued from "/tobecontinued.png"
+import './../styles/Contact.css'
+
+
 export default function Contact()
 {
-    return <><img src={continued}></img></>
+    return (
+        <nav className="contact">
+            <h1>Get in Touch With Me</h1>
+
+            <p>{`
+                I am currently searching for Co-Op/Internship Opportunities for Summer 2025.
+                I respond to emails by at most 2-3 days. Feel free to send an email for all inquries!
+            `}</p>
+
+            <p><strong>Email:</strong> <a href="mailto:ekin.y@dal.ca">ekin.y@dal.ca</a></p>
+            <nav className="icons">
+                <a href="https://github.com/eyurekli">
+                            <i className="devicon-github-plain"/>
+                        </a>
+                <a href="https://www.linkedin.com/in/ekiny/">
+                            <i className="devicon-linkedin-plain"></i>
+                </a>
+            </nav>
+        </nav>
+    )
 }
