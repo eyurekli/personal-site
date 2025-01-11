016f2e903a4b7c13b0007098357bef599773a6d0
yurekli <kurumizawa38@gmail.com>
2025-01-11 15:47:46 -0400

    Updated Project and Contacts

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
-import disaster from "/project_disaster.png"
-import Project from "./Project.jsx"
-import './../styles/Projects.css'
+import disaster from "/project_disaster.png";
+import dio_cairo from "/dios_world_cairo.png";
+import Project from "./Project.jsx";
+import './../styles/Projects.css';
+
 export default function Projects()
 {
 
@@ -13,27 +15,32 @@ export default function Projects()
                 secured 2nd place at the NASA Space Apps Hackathon in Nova Scotia, and selected for global judging.
             `,
             link: "https://community-mapping-cyan.vercel.app/"
+        },
+        {
+            title: "DIO's World",
+            img: dio_cairo,
+            desc: `
+                A 2D fighting game inspired by Capcom's 1998 game, JoJo's Bizarre Adventure: Heritage for the Future.
+                Built in Python with pygame. Player controls DIO to choose from 3 objectives to complete.
+            `,
+            link: "https://github.com/eyurekli/DIOsWorld"
         }
     ];
-
-    
-    const disaster_desc = `
-    Built a natural disaster tracker by integrating real-time data with 3D globe visualization in under 24 hours, 
-    secured 2nd place at the NASA Space Apps Hackathon in Nova Scotia, and selected for global judging.
-    `;
 
     return (
         <>
             <h1>Projects</h1>
-            {projects.map((project, index) => (
-                <Project
-                    key={index}
-                    title={project.title}
-                    img={project.img}
-                    desc={project.desc}
-                    link={project.link}
-                />
-            ))}
+            <div id="projects">
+                {projects.map((project, index) => (
+                    <Project
+                        key={index}
+                        title={project.title}
+                        img={project.img}
+                        desc={project.desc}
+                        link={project.link}
+                    />
+                ))}
+            </div>
         </>
     )
 }
